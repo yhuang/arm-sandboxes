e259a0b10b667b85b2d7bb151c3c0d9d8b26cd32
Jimmy Huang <jimmy.huang@duragility.com>
2023-04-15 19:55:46 -0700

    need to delete AWSCLI_V2_ARCHIVE from homedir before finish building ubuntu-base image

--- ubuntu-22.04-base/provisioning-scripts/install-aws-cli.sh
+++ ubuntu-22.04-base/provisioning-scripts/install-aws-cli.sh
@@ -3,5 +3,5 @@
 AWSCLI_V2_ARCHIVE="awscliv2.zip"
 
 curl https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip -o ${AWSCLI_V2_ARCHIVE}
-unzip ${AWSCLI_V2_ARCHIVE}
-./aws/install
+unzip ${AWSCLI_V2_ARCHIVE} && rm -f ${AWSCLI_V2_ARCHIVE}
+./aws/install && rm -fr aws
